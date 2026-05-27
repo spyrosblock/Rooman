c37fcb02ec2d27f308f194655fdae4d2c03af7a1
spyrosblock <83170047+spyrosblock@users.noreply.github.com>
2026-05-27 13:24:15 +0300

    fix the password hash

--- db/init/004_seed_users.sql
+++ db/init/004_seed_users.sql
@@ -1,3 +1,5 @@
 INSERT INTO users (email, password) VALUES
-('admin@admin.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'), /* password is password */
-('root@root.com',  '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy');
+('admin@admin.com', '$2a$10$s1h5AjKrBzx/OOIQwZPflOWzXp8T7G7bGTxntteaLWujNr4CJKf8u'), /* password is password (strength 10) */
+('root@root.com',  '$2a$10$s1h5AjKrBzx/OOIQwZPflOWzXp8T7G7bGTxntteaLWujNr4CJKf8u');
+
+/* for admin: $2a$10$5VS2DqaRe2obwefZAF2BQO5M7FvJ16l71mX6TKpkZFBeAD2tD5ZxW */
