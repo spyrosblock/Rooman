532abb9d1b20c5c9b62a3ae6ef18ec3caee4350b
spyrosblock <83170047+spyrosblock@users.noreply.github.com>
2026-05-27 20:13:29 +0300

    change min password length so that admin is valid

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -42,8 +42,8 @@ export default function Login() {
 
     if (!password) {
       errs.password = 'Password is required'
-    } else if (password.length < 6) {
-      errs.password = 'Password must be at least 6 characters'
+    } else if (password.length < 5) {
+      errs.password = 'Password must be at least 5 characters'
     }
 
     return errs
