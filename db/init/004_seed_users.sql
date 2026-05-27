b7953bba37acc8775322ecbc5fb14ceedc009ac3
spyrosblock <83170047+spyrosblock@users.noreply.github.com>
2026-05-27 20:08:39 +0300

    change users password

--- db/init/004_seed_users.sql
+++ db/init/004_seed_users.sql
@@ -1,5 +1,6 @@
 INSERT INTO users (email, password) VALUES
-('admin@admin.com', '$2a$10$s1h5AjKrBzx/OOIQwZPflOWzXp8T7G7bGTxntteaLWujNr4CJKf8u'), /* password is password (strength 10) */
-('root@root.com',  '$2a$10$s1h5AjKrBzx/OOIQwZPflOWzXp8T7G7bGTxntteaLWujNr4CJKf8u');
+('admin@admin.com', '$2a$10$5VS2DqaRe2obwefZAF2BQO5M7FvJ16l71mX6TKpkZFBeAD2tD5ZxW'), /* password is admin (strength 10) */
+('root@root.com',  '$2a$10$5VS2DqaRe2obwefZAF2BQO5M7FvJ16l71mX6TKpkZFBeAD2tD5ZxW');
 
 /* for admin: $2a$10$5VS2DqaRe2obwefZAF2BQO5M7FvJ16l71mX6TKpkZFBeAD2tD5ZxW */
+/* for password: $2a$10$s1h5AjKrBzx/OOIQwZPflOWzXp8T7G7bGTxntteaLWujNr4CJKf8u */
