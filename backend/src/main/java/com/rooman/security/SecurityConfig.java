857cdeaabec81c716cabf3ac0332eb0f46c04075
spyrosblock <83170047+spyrosblock@users.noreply.github.com>
2026-05-30 09:01:17 +0300

    chore: remove unnecessary import

--- backend/src/main/java/com/rooman/security/SecurityConfig.java
+++ backend/src/main/java/com/rooman/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import com.rooman.security.SHA256PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
