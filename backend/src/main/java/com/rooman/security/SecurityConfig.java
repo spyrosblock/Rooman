6ce384330c2413d2435810d7db42380364c857c1
spyrosblock <83170047+spyrosblock@users.noreply.github.com>
2026-06-01 05:34:20 +0300

    remove redundant code from SecurityConfig.java

--- backend/src/main/java/com/rooman/security/SecurityConfig.java
+++ backend/src/main/java/com/rooman/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,11 +17,6 @@ public class SecurityConfig {
 
     public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new SHA256PasswordEncoder();
     }
 
     @Bean
